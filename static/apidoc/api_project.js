030232f398254eb4530f46ec3440709a37984fee
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-09 00:38:18 +0600

    test

---
+++ static/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "belka",
+  "version": "0.0.1",
+  "description": "Api",
+  "title": "Belka Documentation",
+  "url": "http://localhost/api",
+  "sampleUrl": "http://localhost/api",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-03-08T18:38:07.761Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
