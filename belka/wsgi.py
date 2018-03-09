7c02431ead61470d0a7f8f79bb37f0efb7aa6f7c
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-09 14:58:19 +0600

    fix for server

--- belka/wsgi.py
+++ belka/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belka.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belka.ocean")
 
 application = get_wsgi_application()
