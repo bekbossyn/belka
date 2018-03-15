befa13cb8972dfe757683649c8452d1ba7d11b19
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-16 02:05:35 +0600

    fix run.shell

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source 'env/bin/activate'
+source '../env/bin/activate'
 pip install -r requirements.txt
 python server.py collectstatic --noinput
 python server.py migrate --noinput
