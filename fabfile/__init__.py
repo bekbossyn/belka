22a59ece914e8f808aa2c9b202316c25f17487aa
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-17 05:18:06 +0600

    fixes fabric

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,11 +1,14 @@
 from . import common
 
 from fabric.state import env
-from fabric.decorators import task, hosts
+from fabric.decorators import task
 from fabric.operations import sudo, run
 
 env.repository = "https://github.com/bekbossyn/belka.git"
 env.repo_name = "belka"
+env.hosts = ["159.65.203.197"]
+env.user = "root"
+env.password = "Truesight7"
 
 
 # @task
@@ -25,14 +28,14 @@ env.repo_name = "belka"
 #
 
 @task
-@hosts(['159.65.203.197'])
+# @hosts(['159.65.203.197'])
 def restart():
     """
         updates repo, restarts the server
     """
-    env.user = "root"
-    env.password = "Truesight7"
     # env.hosts = ["159.65.203.197"]
+    # env.user = "root"
+    # env.password = "Truesight7"
 
     run("cd /root/belka && git pull origin master")
 
