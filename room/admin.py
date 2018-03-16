1a654436ffe928246dd7e2b347da27f2f9bdf17a
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-16 18:47:41 +0600

    admin

--- room/admin.py
+++ room/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Deck, Hand, Card, Room
+from .models import Deck, Hand, Card, Room, Move
 
 
 @admin.register(Room)
@@ -9,26 +9,43 @@ class RoomAdmin(admin.ModelAdmin):
         'pk', 'owner', 'user01', 'user01_ready', 'user02', 'user02_ready', 'user03', 'user03_ready', 'user04',
         'user04_ready', 'all_ready', 'started', 'full', 'active', 'timestamp',)
 
+    list_filter = ('owner', 'started', 'full', 'active',)
+
     readonly_fields = ('timestamp', )
 
 
 @admin.register(Deck)
 class DeckAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'trump', 'next_move', 'total_moves', 'active', 'timestamp',)
+    list_display = ('pk', 'room', 'trump', 'next_move', 'total_moves', 'active', 'timestamp',)
+
+    list_filter = ('room', 'total_moves', 'active',)
 
     readonly_fields = ('timestamp', )
 
 
+@admin.register(Move)
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'deck', 'user', 'card_id', 'first_move', 'active', 'timestamp',)
+
+    list_filter = ('deck', 'user', 'first_move', 'active',)
+
+    readonly_fields = ('timestamp',)
+
+
 @admin.register(Hand)
 class HandAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'deck', 'active', 'timestamp',)
+    list_display = ('pk', 'user', 'deck', 'active', 'timestamp',)
+
+    list_filter = ('user', 'deck', 'active',)
 
     readonly_fields = ('timestamp',)
 
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'hand', 'name', 'value', 'worth', 'image_url', 'trump_priority', 'active', 'timestamp',)
+    list_display = ('pk', 'deck', 'hand', 'name', 'value', 'worth', 'image_url', 'trump_priority', 'active', 'timestamp',)
+
+    list_filter = ('deck', 'hand', 'active',)
 
     readonly_fields = ('timestamp',)
 
