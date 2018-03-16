5f4ce3a072595daf99e5be881ac74dc1b10da253
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-03-17 04:59:35 +0600

    test fabric

--- fabfile/common.py
+++ fabfile/common.py
@@ -2,12 +2,19 @@ from fabric.decorators import task
 from fabric.operations import sudo, run
 from fabric.state import env
 
+env.repository = "https://github.com/bekbossyn/belka.git"
+env.repo_name = "belka"
 
 @task
 def git_pull():
     """
     Updates the repository
     """
+
+    env.user = "root"
+    env.password = "Truesight7"
+    env.hosts = ["159.65.203.197"]
+
     run("cd /root/belka && git pull origin master")
 
 
