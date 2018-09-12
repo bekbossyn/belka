bc33ec53fa65c75dfe0d7bbd322c46204d940dae
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-09-12 19:22:49 +0900

    fix runsh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source 'source /root/dev/envs/belka_env/bin/activate'
+source '/root/dev/envs/belka_env/bin/activate'
 pip install -r requirements.txt
 python server.py collectstatic --noinput
 python server.py migrate --noinput
