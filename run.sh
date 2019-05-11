1e6fc06e80ee0043adc37fc9992f9df4131fd818
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-11 17:29:44 +0900

    foxes

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-#!/usr/bin/env bashsource '/root/dev/envs/belka_env/bin/activate'pip install -r requirements.txtpython server.py collectstatic --noinputpython server.py migrate --noinput
+#!/usr/bin/env bash
+source '/root/dev/envs/belka_env/bin/activate'
+pip install -r requirements.txt
+python server.py collectstatic --noinput
+python server.py migrate --noinput
+
