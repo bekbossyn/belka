3c5acfbba030ff7d7316712829575a0b29aa4bf1
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2019-05-11 13:02:55 +0900

    format change

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/env bash
-source '/root/dev/envs/belka_env/bin/activate'
-pip install -r requirements.txt
-python server.py collectstatic --noinput
-python server.py migrate --noinput
+#!/usr/bin/env bashsource '/root/dev/envs/belka_env/bin/activate'pip install -r requirements.txtpython server.py collectstatic --noinputpython server.py migrate --noinput
